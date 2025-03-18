f9d44cfa2d70dc108beb702e4c4c22b498a9567c
PriyangaRavichandiran <priyanga2941@gmail.com>
2025-03-18 22:53:05 +0530

    Initial commit of my project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoApp from './component/TodoApp';
+import Todo from './component/Todo';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Todo/>
+    </div>
+  );
+}
+
+export default App;
